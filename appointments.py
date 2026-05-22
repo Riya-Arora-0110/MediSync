100f9fd7077c8029cddcb5eae58fc2bc552f2f3b
Riya Arora <ria.arora@mathco.com>
2026-05-22 10:17:21 +0000

    updated appointments with city and state instead of location

--- appointments.py
+++ appointments.py
@@ -1,14 +1,15 @@
 class Appointments:
-    def __init__(self, patient_name, doctor_name, date, time, location):
+    def __init__(self, patient_name, doctor_name, date, time, city, state):
         self.patient_name = patient_name
         self.doctor_name = doctor_name
         self.date = date
         self.time = time
-        self.location = location
+        self.city = city
+        self.state = state
 
     def schedule(self):
-        return f"Appointment scheduled fpr {self.patient_name} with Dr. {self.doctor_name} on {self.date} at {self.time}"
+        return f"Appointment scheduled fpr {self.patient_name} with Dr. {self.doctor_name} on {self.date} at {self.time} in {self.city", {self.state
 
 if __name == "__main__":
-    appointment1 = Appointment("John Doe", "Smith", "2026-05-22", "10:30 AM")
+    appointment1 = Appointment("John Doe", "Smith", "2026-05-22", "10:30 AM", "Mumbai", "Maharashtra")
     print(appointment1.schedule())
